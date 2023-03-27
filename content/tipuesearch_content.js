51e81000c1d2da696d74fc4145c94091efe6ed81
40823218 <40823218@gm.nfu.edu.tw>
2023-03-27 13:03:44 +0800

    image test

--- content/tipuesearch_content.js
+++ content/tipuesearch_content.js
@@ -1,1 +1,1 @@
-var tipuesearch = {"pages": [{'title': 'About', 'text': '3/27test \n', 'tags': '', 'url': 'About.html'}]};
+var tipuesearch = {"pages": [{'title': 'About', 'text': '3/27test \n \n', 'tags': '', 'url': 'About.html'}]};
